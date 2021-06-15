c4853165763dc8efe6fc068273d8fa302ddab4f7
FesenkoE <fesenkoe@ukr.net>
2021-06-16 00:41:41 +0300

    [hw-1] add function fib_sum

--- hw_1/task2.py
+++ hw_1/task2.py
@@ -1,18 +1,19 @@
 # Написать функцию для вычислений очередного числа Фибоначчи (можно через цикл, можно
 # через рекурсию).
 
-f_num_1 = 1
-f_num_2 = 1
 
-# for example n = 7
-n = 7
-i = 1
+def fib_sum(num):
+    f_num_1 = 1
+    f_num_2 = 1
+    i = 1
 
-while i < n - 1:
-    f_summa = f_num_1 + f_num_2
-    f_num_1 = f_num_2
-    f_num_2 = f_summa
+    while i < num - 1:
+        f_summa = f_num_1 + f_num_2
+        f_num_1 = f_num_2
+        f_num_2 = f_summa
+        i += 1
 
-    i += 1
+    return f_num_2
 
-print('Summa fibonachi: ', f_num_2)
+
+print(fib_sum(7))
