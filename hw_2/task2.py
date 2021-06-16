bd63ca8cebbecc194f5767dbd294eb62debe5d43
FesenkoE <fesenkoe@ukr.net>
2021-06-16 23:16:28 +0300

    [hw-2] task 2 done

--- hw_2/task2.py
+++ hw_2/task2.py
@@ -9,4 +9,32 @@
 - метод, изменяющий статус заявки
 - метод, возвращающий id заявки
 """
+import uuid
+from datetime import datetime
 
+
+class Order:
+    def __init__(self, user_name, serial_number, unique_id=uuid.uuid1(), date_time=datetime.now(), status=True):
+        self.user_name = user_name
+        self.__serial_number = serial_number
+        self.unique_id = unique_id
+        self.date_time = date_time
+        self.status = status
+
+    def set_status(self, status):
+        self.status = status
+
+    def get_id(self):
+        return self.unique_id
+
+    def get_active_time(self):
+        if self.status:
+            return datetime.now() - self.date_time
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+
+order = Order('Vasya', 123)
+
+print(order.get_active_time())
