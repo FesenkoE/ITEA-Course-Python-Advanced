fff3d25a4782b10c39cb070d25f62475ae9a8afe
FesenkoE <fesenkoe@ukr.net>
2021-07-05 23:35:24 +0300

    [hw-3] task 2 add check with error

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -37,6 +37,16 @@ def test_multiple_matrix(matrix_1, number, matrix_result):
     ([[2, 2]], 2, '1.0 1.0 \n'),
     ([[8, 10]], 2, '4.0 5.0 \n'),
 ])
-def test_multiple_matrix(matrix_1, number, matrix_result):
+def test_div_matrix(matrix_1, number, matrix_result):
     res = Matrix(matrix_1) / number
     assert res.__str__() == matrix_result
+
+
+@pytest.mark.parametrize("exc_type, matrix_1, number", [
+    (ZeroDivisionError, [[2, 6]], 0),
+    (TypeError, '[[2, 2]]', 0),
+    (ZeroDivisionError, [[8, 10]], 0),
+])
+def test_div_matrix_with_error(exc_type, matrix_1, number):
+    with pytest.raises(exc_type):
+        Matrix(matrix_1) / number
