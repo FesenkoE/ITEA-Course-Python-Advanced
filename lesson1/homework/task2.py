3a3f8e144234cd0e1e9914e4f3fa376a6c18519b
evgenii.fesenko <evgenii.fesenko@eosda.com>
2023-02-24 18:25:46 +0200

    task 2 done

--- lesson1/homework/task2.py
+++ lesson1/homework/task2.py
@@ -1,2 +1,23 @@
 # Написать функцию для вычислений очередного числа Фибоначчи (можно через цикл, можно
 # через рекурсию).
+
+
+def main():
+    def fib_number(n):
+        afp = 0
+        bfn = 1
+        i = 1
+        fib = 1
+        while i < n:
+            fib = afp + bfn
+            afp = bfn
+            bfn = fib
+            i += 1
+
+        return fib
+
+    print(fib_number(6))
+
+
+if __name__ == '__main__':
+    main()
